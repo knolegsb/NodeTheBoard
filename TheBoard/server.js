913305f67ec0ca13ac2e9a7417020e4a971922f0
knolegsb <seon2you@hotmail.com>
2015-10-13 15:31:30 -0700

    try send request json

--- TheBoard/server.js
+++ TheBoard/server.js
@@ -5,12 +5,23 @@
 //    res.end('Hello World\n');
 //}).listen(port);
 
-var http = require('http');
+var http = require("http");
+var express = require("express");
+var app = express();
 
-var server = http.createServer(function (req, res) {
-    console.log(req.url);
-    res.write("<html><body><h1>" + req.url + "</h1></body></html>");
-    res.end();
+app.get("/", function (req, res) {
+    res.send("<html><body><h1>Express</h1></body></html>");
 });
 
+app.get("/api/users", function (req, res) {
+    res.set("Content-Type", "application/json");
+    res.send({ name: "Sean", isValid: true, group: "Admin" });
+});
+//var server = http.createServer(function (req, res) {
+//    console.log(req.url);
+//    res.write("<html><body><h1>" + req.url + "</h1></body></html>");
+//    res.end();
+//});
+
+var server = http.createServer(app);
 server.listen(3000);
