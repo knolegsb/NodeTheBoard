ad114087aea109002cac40cd32550e95a7e00630
knolegsb <seon2you@hotmail.com>
2015-10-13 15:56:34 -0700

    jade view engine

--- TheBoard/server.js
+++ TheBoard/server.js
@@ -9,8 +9,12 @@ var http = require("http");
 var express = require("express");
 var app = express();
 
+// Setup the View Engine
+app.set("view engine", "jade");
+
 app.get("/", function (req, res) {
-    res.send("<html><body><h1>Express</h1></body></html>");
+    //res.send("<html><body><h1>Express</h1></body></html>");
+    res.render("jade/index", { title: "Express + Jade" });
 });
 
 app.get("/api/users", function (req, res) {
