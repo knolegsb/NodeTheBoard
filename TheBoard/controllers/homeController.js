38fed6b53413fac5c06870c700080fdebf8e1d00
knolegsb <seon2you@hotmail.com>
2015-10-14 01:04:09 -0700

    added controller and data

---
+++ TheBoard/controllers/homeController.js
@@ -0,0 +1,16 @@
+﻿(function (homeController) {
+    
+    var data = require("../data");
+
+    homeController.init = function (app) {
+        app.get("/", function (req, res) {
+            //res.send("<html><body><h1>Express</h1></body></html>");
+            //res.render("jade/index", { title: "Express + Jade" });
+            //res.render("ejs/index", { title: "Express + EJS" });
+            
+            data.getNoteCategories(function (err, results) {
+                res.render("index", { title: "Express + Vash", error: err, categories: results });
+            });
+        });
+    };
+})(module.exports);
--- TheBoard/server.js
+++ TheBoard/server.js
@@ -9,6 +9,7 @@ var http = require("http");
 var express = require("express");
 var app = express();
 //var ejsEngine = require("ejs-locals");
+var controllers = require("./controllers");
 
 // Setup the View Engine
 //app.set("view engine", "jade");
@@ -16,12 +17,15 @@ var app = express();
 //app.set("view engine", "ejs"); // ejs view engine
 app.set("view engine", "vash");
 
-app.get("/", function (req, res) {
+// Map the routes
+controllers.init(app);
+
+//app.get("/", function (req, res) {
     //res.send("<html><body><h1>Express</h1></body></html>");
     //res.render("jade/index", { title: "Express + Jade" });
     //res.render("ejs/index", { title: "Express + EJS" });
-    res.render("index", {title: "Express + Vash"})
-});
+    //res.render("index", {title: "Express + Vash"})
+//});
 
 app.get("/api/users", function (req, res) {
     res.set("Content-Type", "application/json");
